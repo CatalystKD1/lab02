fdcf90b02dda9abf5b7e7b260e2875908a8ad199
Jordan McCrank <81048903+InsertFunny@users.noreply.github.com>
2022-03-03 22:24:08 +0000

    Finished parts 1-3

---
+++ main.py
@@ -0,0 +1,36 @@
+def part1():
+  #This is for part 1
+  n = 42
+  tau = 3.13159
+  school = "Sault College"
+  print(n)
+  print(tau)
+  print(school)
+  print("Part 1 completed!")
+  
+def part2():
+  #This is for part 2  
+  #class = "CSD110"        
+  #desired-grade = 100     
+  #2hot = "hothot"
+  #3.14159 = pi
+  _class = "CSD110" #class is a part of the python language. Change name
+  desired_grade = 100 #Tried to subtracted desired by grade
+  hot = "hothot" #Can't have a number for/at the begining of, a variable name
+  pi = 3.14159 # pi and the numbres should be switched
+  print(_class)
+  print(desired_grade)
+  print(hot)
+  print(pi)
+  print("Part 2 is complete!")
+  
+def part3():
+  print("Feeling... ", end = "")
+  i = 0
+  while (i < 3):
+    print("Hot ", end = "")
+    i += 1
+  print("Part 3 is complete!")
+
+def part4():
+  
